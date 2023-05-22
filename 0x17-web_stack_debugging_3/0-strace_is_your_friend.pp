d1cb0970535d209fd8a4b4caae89136e77bb5182
MosabUI <108566559+MosabUI@users.noreply.github.com>
2023-05-22 04:04:19 +0400

    Add files via upload

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# puppet file to automate a 500 error fix
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+# Using strace, find out why Apache is returning a 500 error and fix
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
